5b5a2f10da51d30cb541822570f884e37571e3da
Darwin Nifras <130158249+Durwen21@users.noreply.github.com>
2025-05-12 18:52:38 +0800

    The most basic component done

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:night_walkers_app/widgets/panic_button.dart';
+import 'package:night_walkers_app/screens/settings_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _screens = <Widget>[
+    Center(child: PanicButton()),
+    SettingsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Night Walkers App'),
+        title: _selectedIndex == 0
+          ? const Text('Night Walkers App')
+          : null,
+        
       ),
-      body: Center(
-        child: PanicButton(),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/panic_button.dart
+++ lib/widgets/panic_button.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 
 class PanicButton extends StatelessWidget {
+  const PanicButton({super.key});
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: const CircleBorder(), backgroundColor: Colors.red,
+        padding: const EdgeInsets.all(60), 
+      ),
       onPressed: () {
         // Trigger SOS functionality
       },
-      child: const Text('SOS'),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Icon(
+            Icons.warning,
+            color: Colors.white,
+            size: 90,
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'SOS',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 90,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
